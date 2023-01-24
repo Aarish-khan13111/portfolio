de6e5340d70c23536fa1cbab8ed69f929cf63a2e
Aarish khan <aarishkhan13111@gmail.com>
2023-01-24 17:55:01 +0530

    add a deployed link of spotify proj

--- pages/spotify.jsx
+++ pages/spotify.jsx
@@ -50,6 +50,12 @@ const property = () => {
               rel="noreferrer">
               <button className="px-8 py-2 mt-4 mr-8">Code</button>
             </a>
+            <a
+              href="https://soptify-app-mak.vercel.app"
+              target="_blank"
+              rel="noreferrer">
+              <button className="px-8 py-2 mt-4 mr-8">Demo</button>
+            </a>
           </div>
           <div className="col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl py-4">
             <div className="p-2">
